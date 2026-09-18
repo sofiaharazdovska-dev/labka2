de3c5a7bd8a98281a2c147cdc40e5495e16dda4f
Sofia <sofia.haraz@gmail.com>
2026-09-18 18:19:03 +0300

    Initial commit: lab project files

---
+++ AP_2.123.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+//text for commit
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!" << endl;
+
+    int p = 1;
+    int i = 1;
+    // feature branch test
+// feature branch test
+    
+    while (i <= 10) 
+    {
+        p *= i;
+        i++;
+    }
+   // second test line for PR
+    cout << "p = " << p << " i = " << i << endl;
+// testing 2
+    // Замість system("pause") для Mac:
+    cin.get(); 
+    return 0;
+}
+// test PR change
